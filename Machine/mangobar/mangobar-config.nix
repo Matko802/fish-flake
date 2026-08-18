6341caaccf1fd53c670129e2d00df946b133f36d
Matko802 <matkohrad@proton.me>
2026-08-18 13:22:53 +0200

    Remove mangobar, neovim, and virtualbox configs
    The helix module is simplified to install the `helix` package and
    symlink an external config directory in place of the previous inline
    `programs.helix` settings.

--- Machine/mangobar/mangobar-config.nix
+++
@@ -1,9 +0,0 @@
-{ config, inputs, pkgs, ... }: {
-  systemd.tmpfiles.rules = [
-    "L+ ${config.users.users.matko.home}/.config/mangobar - - - - ${./config}"
-  ];
-
-  environment.systemPackages = [
-    inputs.mangobar.packages.x86_64-linux.mangobar
-  ];
-}
--- Machine/virtualbox/virtualbox-config.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, ... }: {
-  systemd.tmpfiles.rules = [
-    "L+ ${config.users.users.matko.home}/.config/VirtualBox/Qt.conf - - - - ${./config/Qt.conf}"
-  ];
-}
